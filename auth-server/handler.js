c9cadcb568b2076bdd0bec7c3bd26ec465924d6e
may_ <lammayyin@gmail.com>
2024-08-18 00:33:36 +0200

    add getCalendarEvents() in handler.js and test-auth-server.html

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -77,7 +77,8 @@ module.exports.getAccessToken = async (event) => {
 //getCalendarEvents lambda function
 module.exports.getCalendarEvents = async (event) => {
 
-  const access_token = oAuth2Client.setCredentials({ access_token });
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+  oAuth2Client.setCredentials({ access_token });
 
   return new Promise((resolve, reject) => {
 
