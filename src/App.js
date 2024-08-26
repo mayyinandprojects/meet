f926042756de644d2dc0a72b0abc91ba71b16c2b
may_ <lammayyin@gmail.com>
2024-08-27 00:51:18 +0200

    debug number of events
    debug issue where number of events displayed is not reflected when there is a changed in the number of events input box

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-// src/App.js
-
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import './App.css';
 
 const App = () => {
@@ -16,63 +12,26 @@ const App = () => {
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
 
-
-
   const fetchData = async () => {
     const allEvents = await getEvents();
     const filteredEvents = currentCity === "See all cities" ?
       allEvents :
-      allEvents.filter(event => event.location === currentCity)
-    setEvents(filteredEvents.slice(0, currentNOE));
+      allEvents.filter(event => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE)); // Slice based on currentNOE
     setAllLocations(extractLocations(allEvents));
   }
 
   useEffect(() => {
     fetchData();
-  }, [currentCity]);
+  }, [currentCity, currentNOE]); // Add currentNOE here
 
   return (
     <div className="App">
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents currentNOE={currentNOE} setCurrentNOE={setCurrentNOE}/>
+      <NumberOfEvents currentNOE={currentNOE} setCurrentNOE={setCurrentNOE} />
       <EventList events={events} />
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-// const App = () => {
-//   const [allLocations, setAllLocations] = useState([]);
-//   const [events, setEvents] = useState([]);
-//   const [numberOfEvents, setNumberOfEvents] = useState(32);
-
-//   useEffect(() => {
-//     const fetchEvents = async () => {
-//       const events = await getEvents();
-//       const locations = extractLocations(events);
-//       setAllLocations(locations);
-//       setEvents(events.slice(0, numberOfEvents));
-//     };
-
-//     fetchEvents();
-//   }, [numberOfEvents]);
-
-//   const handleNumberOfEventsChange = (event) => {
-//     setNumberOfEvents(event.target.value);
-//   };
-
-//   return (
-//     <div className="App">
-//       <CitySearch allLocations={allLocations} />
-//       <NumberOfEvents numberOfEvents={numberOfEvents} onNumberOfEventsChange={handleNumberOfEventsChange} />
-//       <EventList events={events} />
-//     </div>
-//   );
-// }
-
-// export default App;
